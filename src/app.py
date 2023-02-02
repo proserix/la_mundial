1e8c1820f232feec5d08f8a16ea473f3898ccf31
sponcedeleon <proserix@gmail.com>
2023-02-02 08:54:11 -0300

    updated code app

--- src/app.py
+++ src/app.py
@@ -31,82 +31,82 @@ df = pd.read_pickle('data_la_mundial_2022.pickle')
 
 
 def facturacion_detalle(data):
-	map_day_name = {
-        'Monday'   :'01_Monday',
-        'Tuesday'  :'02_Tuesday',
-        'Wednesday':'03_Wednesday',
-        'Thursday' :'04_Thursday',
-        'Friday'   :'05_Friday',
-        'Saturday' :'06_Saturday',
-        'Sunday'   :'07_Sunday'
-	}
-	map_month_name = {
-        'January':'01_January',
-        'February':'02_February',
-        'March':'03_March',
-        'April':'04_April',
-        'May':'05_May',
-        'June':'06_June',
-        'July':'07_July',
-        'August':'08_August',
-        'September':'09_September',
-        'October':'10_October',
-        'November':'11_November',
-        'December':'12_December'
-	}
-	df = data            .reset_index()            .groupby('fecha')            .agg({'precio_total_con_iva':sum})            .assign(date = lambda df_: df_.index.date)            .groupby('date')            .agg({'precio_total_con_iva':sum})            .asfreq('D')            .assign(day_name = lambda df_: df_.index.day_name())            .assign(month_name = lambda df_: df_.index.month_name())            .assign(day = lambda df_: df_.index.day)
-	df.day_name   = df.day_name.map(map_day_name)
-	df.month_name = df.month_name.map(map_month_name)
+    map_day_name = {
+            'Monday'   :'01_Monday',
+            'Tuesday'  :'02_Tuesday',
+            'Wednesday':'03_Wednesday',
+            'Thursday' :'04_Thursday',
+            'Friday'   :'05_Friday',
+            'Saturday' :'06_Saturday',
+            'Sunday'   :'07_Sunday'
+    }
+    map_month_name = {
+            'January':'01_January',
+            'February':'02_February',
+            'March':'03_March',
+            'April':'04_April',
+            'May':'05_May',
+            'June':'06_June',
+            'July':'07_July',
+            'August':'08_August',
+            'September':'09_September',
+            'October':'10_October',
+            'November':'11_November',
+            'December':'12_December'
+    }
+    df = data.reset_index().groupby('fecha').agg({'precio_total_con_iva':sum}).assign(date = lambda df_: df_.index.date)            .groupby('date')            .agg({'precio_total_con_iva':sum})            .asfreq('D')            .assign(day_name = lambda df_: df_.index.day_name())            .assign(month_name = lambda df_: df_.index.month_name())            .assign(day = lambda df_: df_.index.day)
+    df.day_name   = df.day_name.map(map_day_name)
+    df.month_name = df.month_name.map(map_month_name)
 
-    # ------------------------------------------------
-	condicion_tercio_mes = [
-        (df.day <= 10),
-        (df.day >10) & (df.day<=20),
-        (df.day >20)
-	]
+        # ------------------------------------------------
+    condicion_tercio_mes = [
+            (df.day <= 10),
+            (df.day >10) & (df.day<=20),
+            (df.day >20)
+    ]
 
-	resultado_t = ['tercio_1', 'tercio_2', 'tercio_3']
-    # ------------------------------------------------
-	condicion_semana_mes = [
-        (df.day <= 7),
-        (df.day >7)  & (df.day<=14),
-        (df.day >14) & (df.day<=21),
-        (df.day >21) & (df.day<=28),
-        (df.day >28)
-	]
+    resultado_t = ['tercio_1', 'tercio_2', 'tercio_3']
+        # ------------------------------------------------
+    condicion_semana_mes = [
+            (df.day <= 7),
+            (df.day >7)  & (df.day<=14),
+            (df.day >14) & (df.day<=21),
+            (df.day >21) & (df.day<=28),
+            (df.day >28)
+    ]
 
-	resultado_s = ['sem_1', 'sem_2', 'sem_3', 'sem_4', 'sem_5']
-    # ------------------------------------------------
+    resultado_s = ['sem_1', 'sem_2', 'sem_3', 'sem_4', 'sem_5']
+        # ------------------------------------------------
 
-	df['tercio_mes'] = np.select(condicion_tercio_mes, resultado_t, default=-999)
-	df['semana_mes'] = np.select(condicion_semana_mes, resultado_s, default=-999)
+    df['tercio_mes'] = np.select(condicion_tercio_mes, resultado_t, default=-999)
+    df['semana_mes'] = np.select(condicion_semana_mes, resultado_s, default=-999)
 
-	return df
+    return df
 # =======================================================================================
 def consumo_detalle(data):	
-	df = data.reset_index().groupby(['fecha','familia']).agg({'cantidad':sum}).unstack(level=1).fillna(0)
-    	df.columns = df.columns.droplevel(0)
-    	return df
+    df = data.reset_index().groupby(['fecha','familia']).agg({'cantidad':sum}).unstack(level=1).fillna(0)
+    df.columns = df.columns.droplevel(0)
+    return df
 # =======================================================================================
 def pv_facturacion(data, rows, cols):
     
-    	df = pd.pivot_table(data   = facturacion,
+    df = pd.pivot_table(data   = facturacion,
                         index  = rows,
                         columns= cols,
                         values ='precio_total_con_iva',
                         aggfunc=lambda x: np.round(np.sum(x)/1000,2)).fillna(0)
-    	return df
+    return df
 # =======================================================================================
 def grafico_hm(df):
-    	# 'RdBu_r', 'inferno', 'Bluered_r', 'px.colors.cyclical.IceFire'
-    	scale_color = px.colors.cyclical.IceFire
-    	fig = px.imshow(df, text_auto=True, aspect="auto",color_continuous_scale=scale_color, origin='upper')
-    	return fig
+    # 'RdBu_r', 'inferno', 'Bluered_r', 'px.colors.cyclical.IceFire'
+    scale_color = px.colors.cyclical.IceFire
+    fig = px.imshow(df, text_auto=True, aspect="auto",color_continuous_scale=scale_color, origin='upper')
+    return fig
 # =======================================================================================
 def corr_hm(df):
-    	# 'RdBu_r', 'inferno'
-    	fig = px.imshow(df, text_auto=True, aspect="auto",color_continuous_scale='RdBu_r', origin='upper')
-    	return fig
+    # 'RdBu_r', 'inferno'
+    fig = px.imshow(df, text_auto=True, aspect="auto",color_continuous_scale='RdBu_r', origin='upper')
+    return fig
 
 
 facturacion = facturacion_detalle(df)
@@ -120,7 +120,7 @@ pv_fact_sem_mes = pv_facturacion(data, 'semana_mes', 'month_name')
 
 def render_table(df):    
 
-    	columns=[
+    columns=[
                 {
                     "name"      : i,
                     "id"        : i,
@@ -129,9 +129,9 @@ def render_table(df):
                     "hideable"  : True
                 } for i in df.columns]
     
-    	font_size = 12
+    font_size = 12
     
-    	table_content =  dash_table.DataTable(
+    table_content =  dash_table.DataTable(
                                 id                ="data_table",
                                 columns           =columns,
                                 data              =df.to_dict("records"),
@@ -174,7 +174,7 @@ def render_table(df):
                                 }
                             )
     
-    	return table_content
+    return table_content
 # =======================================================================================
 card_table = dbc.Row([
     dbc.Card([
