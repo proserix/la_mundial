8aa997624e803764858a743f3b79be1ef321786e
Sebastian Gabriel Ponce de Leon <proserix@gmail.com>
2024-01-19 19:37:17 -0300

    updated

--- app.py
+++ app.py
@@ -1531,4 +1531,5 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
 
 
 if __name__ == '__main__':
-    app.run_server(port=8002, debug=True)
+    # app.run_server(port=8002, debug=True)
+    app.run(debug=False)
